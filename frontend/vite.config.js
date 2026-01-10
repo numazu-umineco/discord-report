e7541cf659bfb84d8c610798744e4d3bc7e4b876
windyakin <windyakin@gmail.com>
2026-01-10 09:37:24 +0900

    ルーティングの修正

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true
       },
-      '^/auth/(?!callback)': {
+      '/auth': {
         target: 'http://localhost:3000',
         changeOrigin: true
       }
